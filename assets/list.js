509723a963b673201a63908f1341721dd0a93654
Bruno Sánchez-Andrade Nuño <brunosan@gmail.com>
2015-07-22 21:06:17 +0200

    JS linting.

--- assets/list.js
+++ assets/list.js
@@ -1,5 +1,5 @@
 // Base URL
-dataurl = "//"+window.location.host+"/web/data/"
+dataurl = "//"+window.location.host+"/web/data/";
 //Read Goals, Targets and Indicators
 var sdgs = [];
 var stats = {};
@@ -10,8 +10,8 @@ function load_data(){
   sdgs = [];
   stats = {};
   files.forEach(function (f) {
-  console.log("Loading "+f);
-  d3.json(dataurl+f+".json", function (error, data) {
+    console.log("Loading "+f);
+    d3.json(dataurl+f+".json", function (error, data) {
     //for (var attrname in data) { sdgs[attrname] = data[attrname]; }
     sdgs.push(data);
     if (sdgs.length==3) { data_loaded(sdgs); }
@@ -32,17 +32,17 @@ function data_loaded(sdgs){
 }
 
 function update_stats(sdgs){
- document.getElementById("goals-num").innerHTML = sdgs[0]["goals"].length+" Goals";
- document.getElementById("targets-num").innerHTML = sdgs[1]["targets"].length+" Targets";
- document.getElementById("indicators-num").innerHTML = sdgs[2]["indicators"].length+" Indicators";
  // Add left bar with numbers.
+ document.getElementById("goals-num").innerHTML = sdgs[0].goals.length+" Goals";
+ document.getElementById("targets-num").innerHTML = sdgs[1].targets.length+" Targets";
+ document.getElementById("indicators-num").innerHTML = sdgs[2].indicators.length+" Indicators";
  add_stack_plot(stats);
 }
 
 function add_stack_plot(stats){
  // Add plot stack viz on id "barplot".
  var data = {
-    labels: d3.range(1, sdgs[0]["goals"].length+1).map(function(i){return "Goal "+i}),
+    labels: d3.range(1, sdgs[0].goals.length+1).map(function(i){return "Goal "+i;}),
     datasets: [
         {
             label: "Targets",
@@ -50,7 +50,7 @@ function add_stack_plot(stats){
             strokeColor: "rgba(220,220,220,0.8)",
             highlightFill: "rgba(220,220,220,0.75)",
             highlightStroke: "rgba(220,220,220,1)",
-            data: stats["goal_targets"] 
+            data: stats.goal_targets
         },
         {
             label: "Indicators",
@@ -58,7 +58,7 @@ function add_stack_plot(stats){
             strokeColor: "rgba(151,187,205,0.8)",
             highlightFill: "rgba(151,187,205,0.75)",
             highlightStroke: "rgba(151,187,205,1)",
-            data: stats["goal_indicators"] 
+            data: stats.goal_indicators
         }
     ]
  };
@@ -77,23 +77,23 @@ function add_stack_plot(stats){
 }
 
  var canvas = document.getElementById("barplot")
- var ctx= canvas.getContext("2d"); 
+ var ctx= canvas.getContext("2d");
  Chart.defaults.global.multiTooltipTemplate = "<%= value %> <%= datasetLabel %> ";
  var myBarChart = new Chart(ctx).Bar(data, options);
- 
-} 
-  
-  
-  
-  
-  
+
+}
+
+
+
+
+
 function list_goals(sdgs){
-  var sdgList = document.getElementById("sdgList"); 
-  var goals=sdgs[0]["goals"];
-  stats["goals"]=goals.length;
+  var sdgList = document.getElementById("sdgList");
+  var goals=sdgs[0].goals;
+  stats.goals=goals.length;
   for (var i in goals){
     var goal= goals[i];
-    append('li',sdgList,"goal-"+goal["goal"],goal["goal"]+": "+goal["title"]);
+    append('li',sdgList,"goal-"+goal.goal,goal.goal+": "+goal.title);
   }
 }
 
@@ -120,56 +120,53 @@ function array_num(size,num){
 }
 
 function add_targets(sdgs){
-  var targets=sdgs[1]["targets"];
-  stats["targets"] = targets.length;
-  stats["goal_targets"] = array_num(targets.length,0);
+  var targets=sdgs[1].targets;
+  stats.targets = targets.length;
+  stats.goal_targets = array_num(targets.length,0);
   for (var i in targets){
     var target=targets[i];
-    stats["goal_targets"][target["goal"]-1]++;
-    var goalLi = document.getElementById("goal-"+target["goal"]);
-    var targetId="goal-"+target["goal"]+"-targets";
-    if ( document.getElementById(targetId) == null) {
+    stats.goal_targets[target.goal-1]++;
+    var goalLi = document.getElementById("goal-"+target.goal);
+    var targetId="goal-"+target.goal+"-targets";
+    if ( document.getElementById(targetId) === null) {
       var nestedOl = document.createElement("ul");
       nestedOl.setAttribute("id", targetId);
       goalLi.appendChild(nestedOl);
     }else{
       var goalLiUl = document.getElementById(targetId);
-      append('li',goalLiUl,"target-"+target["id"],target["id"]+": "+target["title"]);
+      append('li',goalLiUl,"target-"+target.id,target.id+": "+target.title);
     }
   }
 }
 
 function add_indicators(sdgs){
-  var indicators=sdgs[2]["indicators"];
-  stats["indicators"] = indicators.length;
-  stats["goal_indicators"] = array_num(indicators.length,0);
-  
+  var indicators=sdgs[2].indicators;
+  stats.indicators = indicators.length;
+  stats.goal_indicators = array_num(indicators.length,0);
+
   for (var i in indicators){
-    var indicator=indicators[i]; 
-    stats["goal_indicators"][indicator["goal"]-1]++;
-    var goalLi = document.getElementById("goal-"+indicator["goal"]);
-    var indicatorsId="goal-"+indicator["goal"]+"-indicators";
-    if (document.getElementById(indicatorsId) == null) {
+    var indicator=indicators[i];
+    stats.goal_indicators[indicator.goal-1]++;
+    var goalLi = document.getElementById("goal-"+indicator.goal);
+    var indicatorsId="goal-"+indicator.goal+"-indicators";
+    if (document.getElementById(indicatorsId) === null) {
       var responsiveTable = document.createElement("div");
       responsiveTable.setAttribute("class","table-responsive");
       var nestedTable = document.createElement("table");
       nestedTable.setAttribute("class","table table-striped table-bordered");
       var header = nestedTable.createTHead();
       var rowObject = header.insertRow(0);
-      var columns=["Indicator","Leads","Available"]
-      for (var i in columns ){
+      var columns=["Indicator","Leads","Available"];
+      for (var ii in columns ){
           var th = document.createElement('th');
-          th.innerHTML = columns[i];
+          th.innerHTML = columns[ii];
           rowObject.appendChild(th);
-      }   
+      }
       var body = nestedTable.createTBody();
       body.setAttribute("id", indicatorsId);
       goalLi.appendChild(responsiveTable).appendChild(nestedTable);
      }else{
-      append_row(indicatorsId,[indicator["indicator"],indicator["leads"],indicator["available"]]);
+      append_row(indicatorsId,[indicator.indicator,indicator.leads,indicator.available]);
     }
   }
 }
-
-
-
