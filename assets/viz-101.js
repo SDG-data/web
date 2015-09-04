4bafb8c5cb5198dbcee80c868df26bd370e3d0de
Bruno Sánchez-Andrade Nuño <brunosan@gmail.com>
2015-09-04 18:19:26 -0400

    Load viz and place on goal

--- assets/viz-101.js
+++ assets/viz-101.js
@@ -5,6 +5,7 @@ $( document ).ready(load_data());
 function page_main(){
   add_goals_sidebar();
   add_goals();
+  load_vizrefs();
 }
 
 function add_goals_sidebar(){
@@ -24,4 +25,19 @@ function add_goals(){
     var goal= goals[i];
     append('div',sdgList,"goal-"+goal.goal,"h3"," "+goal.goal+": "+goal.title+".");
   }
+}
+
+var visualizations={};
+function load_vizrefs(){
+  // Load Visualizations references from the json file
+  d3.json("/web/assets/viz-101.json", function (error, data) {
+    var viz=data.visualizations[0];
+    var vizHook = document.getElementById("goal-"+viz.goal);
+    console.log(viz,vizHook);
+    if (vizHook === null){
+      throw new Error("Something went badly wrong!");
+    }
+    //append(htype,hookElement,id,classes,value,link)
+    append('div',vizHook,"viz-"+viz.goal,"",viz["viz-title"]);
+  });
 }
